49bafa5d587125eeb58e7750f97fe64ad24723c1
Vignesh Kumar <xxvignesh@gmail.com>
2020-12-17 02:32:59 +0530

    add logging, cron job and notifications

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-extern crate filecmp;
 extern crate fern;
+extern crate filecmp;
+extern crate job_scheduler;
+extern crate notify_rust;
 extern crate regex;
 extern crate whois_rust;
 
 use std::fs;
 use std::path::Path;
 use std::process;
+use std::time::Duration;
 
 use chrono;
+use job_scheduler::{Job, JobScheduler};
 use log::{error, info};
+use notify_rust::Notification;
 use whois_rust::{WhoIs, WhoIsLookupOptions};
 
+// set up logging
 fn setup_logger() -> Result<(), fern::InitError> {
     fern::Dispatch::new()
         .format(|out, message, record| {
@@ -23,7 +29,7 @@ fn setup_logger() -> Result<(), fern::InitError> {
             ))
         })
         .level(log::LevelFilter::Debug)
-        // .chain(std::io::stdout())
+        .chain(std::io::stdout())
         .chain(fern::log_file("log.txt")?)
         .apply()?;
     Ok(())
@@ -35,13 +41,22 @@ const OLD_RESULT_FILE: &str = "./old-result.txt";
 const SERVER_FILE: &str = "./src/servers.json";
 const DOMAIN_ADDRESS: &str = "vignesh.co";
 
-fn quit_error(_e: std::io::Error) {
+fn quit_error(error: std::io::Error) {
+    error!("{}", error);
     process::exit(-1);
 }
 
-fn main() -> std::io::Result<()> {
-    setup_logger().expect("Couldn't setup logging");
+fn requirements() -> bool {
+    match Path::new(SERVER_FILE).exists() {
+        true => return true,
+        false => {
+            error!("Could't find server file at {}", SERVER_FILE);
+            return false;
+        }
+    }
+}
 
+fn who_is() -> std::io::Result<()> {
     let whois = WhoIs::from_path(SERVER_FILE).unwrap();
 
     let mut result: String = whois
@@ -64,7 +79,7 @@ fn main() -> std::io::Result<()> {
     };
 
     match fs::write(RESULT_FILE, result) {
-        Ok(_) => info!("Wrote into result.txt file."),
+        Ok(_) => {}
         Err(e) => {
             error!("Couldn't write into file!");
             quit_error(e);
@@ -73,9 +88,44 @@ fn main() -> std::io::Result<()> {
 
     match filecmp::cmp(RESULT_FILE, OLD_RESULT_FILE, false) {
         Ok(true) => info!("WHOIS is the same."),
-        Ok(false) => info!("WHOIS has changed!"),
+        Ok(false) => {
+            info!("WHOIS has changed!");
+
+            Notification::new()
+                .summary("WHOIS Track")
+                .body("Found a WHOIS change for the domain!")
+                .show()
+                .unwrap();
+        }
         Err(_) => error!("Error while comparing files."),
     }
 
     Ok(())
+}
+
+fn main() {
+    setup_logger().expect("Couldn't set up logging.");
+
+    match requirements() {
+        true => {}
+        false => {
+            quit_error(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "Couldn't find the required files.",
+            ));
+        }
+    }
+
+    let mut sched = JobScheduler::new();
+
+    sched.add(Job::new("0 * * * * *".parse().unwrap(), || {
+        info!("Looking up WHOIS info...");
+        who_is().expect("Couldn't check WHOIS");
+    }));
+
+    loop {
+        sched.tick();
+
+        std::thread::sleep(Duration::from_millis(500));
+    }
 }
